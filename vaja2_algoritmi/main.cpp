24bd5df388beb89b3c5d2f8299a6d91990f673dd
pavel-medved <pavel.medved@student.um.si>
2024-04-09 10:59:50 +0200

    posodobil teste

--- vaja2_algoritmi/main.cpp
+++ vaja2_algoritmi/main.cpp
@@ -4,36 +4,38 @@
 #include <vector>
 #include <cstdint>
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 ofstream out;
 
 string inputText(const string &pot) {
-	ifstream input(pot);
-	stringstream sstream;
+    ifstream input(pot);
+    stringstream sstream;
 
-	if (!input.is_open()) {
-		return string();
-	}
-	
-	sstream << input.rdbuf();
-	return sstream.str();
+    if (!input.is_open()) {
+        return string();
+    }
+
+    sstream << input.rdbuf();
+    return sstream.str();
 }
 
-void izpis_KMPnext(const int* polje, int len) {
-	for (int i = 0; i < len; ++i) {
-		out << polje[i] << ' ';
-	}
-	out << endl;
+void izpis_KMPnext(const int *polje, int len) {
+    for (int i = 0; i < len; ++i) {
+        out << polje[i] << ' ';
+    }
+
 }
 
-void KMP(const string& text, const string& vzorec) {
-	vector<int> kmpNext;
+void KMP(const string &text, const string &vzorec) {
+    vector<int> kmpNext;
     vector<int> index;
     kmpNext.push_back(-1);
     kmpNext.push_back(0);
 
-	int prefix = 1;
+    int prefix = 1;
 
     for (int i = 2; i < vzorec.size(); i++) {
         vector<char> firstPrefix;
@@ -61,7 +63,7 @@ void KMP(const string& text, const string& vzorec) {
         prefix++;
     }
 
-	int j = 0;
+    int j = 0;
     while (j + vzorec.size() <= text.size()) {
         for (int i = 0; i < vzorec.size(); i++) {
             if (vzorec[i] != text[j + i]) {
@@ -76,16 +78,16 @@ void KMP(const string& text, const string& vzorec) {
         }
     }
 
-	 int *polje=new int[index.size()];
-    for(int i=0;i<index.size();i++)
-    {
-        polje[i]=index[i];
+    int *polje = new int[index.size()];
+    for (int i = 0; i < index.size(); i++) {
+        polje[i] = index[i];
     }
     izpis_KMPnext(polje, index.size());
-    for(int i=0;i<kmpNext.size();i++)
-    {
-        out<<kmpNext[i];
+    for (int i = 0; i < kmpNext.size(); i++) {
+        out << kmpNext[i];
+
     }
+    out.close();
     delete[]polje;
 }
 
@@ -95,12 +97,13 @@ void sundaySearch(const string &text, const string &vzorec) { //priprava tabele
     for (int i = 0; i < vzorec.size(); i++) {
         BCH[static_cast<uint8_t>(vzorec[i])] = vzorec.size() - i;
     }
-	int j = 0;
+    int j = 0;
+
     while (j + vzorec.size() <= text.size()) {
         for (int i = 0; i < vzorec.size(); i++) {
             if (vzorec[i] != text[j + i]) {
 
-                j += BCH[text[j + vzorec.size()]];
+                j += BCH[static_cast<uint8_t>(text[j + vzorec.size()])];
                 break;
             }
             if (i + 1 == vzorec.size()) {
@@ -109,38 +112,62 @@ void sundaySearch(const string &text, const string &vzorec) { //priprava tabele
             }
         }
     }
-	int *polje=new int[index.size()];
-    for(int i=0;i<index.size();i++)
-    {
-        polje[i]=index[i];
+    int *polje = new int[index.size()];
+    for (int i = 0; i < index.size(); i++) {
+        polje[i] = index[i];
     }
     izpis_KMPnext(polje, index.size());
     for (int i = 0; i < vzorec.size(); i++) {
-        out<<BCH[static_cast<uint8_t>(vzorec[i])];
+        out << BCH[static_cast<uint8_t>(vzorec[i])];
     }
+    out.close();
     delete[] polje;
 }
 
+void test(const string &outputFile) {
+    string result = inputText(outputFile);
+    string algorithm = inputText("out.txt");
+    ofstream errorFile("napaka.txt");
+    if (result != algorithm) {
+        if (errorFile.is_open()) {
+            errorFile << "napaka" << endl;
+            errorFile.close();
+        } else {
+            if (errorFile.is_open()) {
+                errorFile << "pravilno" << endl;
+                errorFile.close();
+            }
+        }
+    }
+}
+
 int main(int argc, const char *const argv[]) {
-	if (argc != 4) {
-		return -1;
-	}
 
-	string text = inputText(argv[3]);
-	string vzorec = argv[2];
-	out.open("out.txt");
+    string text;
+    string vzorec;
+    if (argc == 4) {
+        text = inputText(argv[3]);
+        vzorec = argv[2];
+    } else if (argc == 2) {
+        text = inputText("test-besedilo.txt");
+        vzorec = "znanstveniki";
+    }
+    out.open("out.txt");
 
-	if (!out) {
-		return -2;
-	}
+    if (!out) {
+        return -2;
+    }
 
-	if (argv[1][0] == '0') {
-		KMP(text, vzorec);
-	}
-	else {
-		sundaySearch(text, vzorec);//Sunday
-	}
-
-	
-	return 0;
+    if (argv[1][0] == '0') {
+        KMP(text, vzorec);
+    } else if (argv[1][0] == '1') {
+        sundaySearch(text, vzorec);//Sunday
+    } else if (strcmp(argv[1], "test_kmp") == 0) {
+        KMP(text, vzorec);
+        test("rezultat-kmpNext.txt");
+    } else if (strcmp(argv[1], "test_sunday") == 0) {
+        sundaySearch(text, vzorec);
+        test("rezultat-sundaySearch.txt");
+    }
+    return 0;
 }
